4e193bdba81eaba7148f2320dd85f2907c9d397b
Gabriel Santos <o.gabriel@aluno.ufabc.edu.br>
2019-05-27 15:25:06 -0300

    Adding a sing up function (non structured) and change the some icons (27052019.3)

--- app/src/main/java/com/example/scannf/MainActivity.java
+++ app/src/main/java/com/example/scannf/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.scannf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnSingUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSingUp = findViewById(R.id.main_btn_sing_up);
+
+        btnSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+
+    public void openActivity() {
+        Intent i = new Intent(this, SingUpActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/scannf/SingUpActivity.java
@@ -0,0 +1,70 @@
+package com.example.scannf;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuickNotesMainActivity";
+    private Button btnSingUp;
+    private EditText etEmail, etPass;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+
+        btnSingUp = findViewById(R.id.singup_btn_sing_up);
+        etEmail = findViewById(R.id.signup_email);
+        etPass = findViewById(R.id.singup_password);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                singup();
+            }
+        });
+
+    }
+
+    public void singup() {
+        etEmail = findViewById(R.id.signup_email);
+        etPass = findViewById(R.id.singup_password);
+        mAuth.createUserWithEmailAndPassword("gabriel.o_santos@yahoo.com.br", "189440")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SingUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+}
